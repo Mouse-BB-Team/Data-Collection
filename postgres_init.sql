93172af7dfcbc137e7e44f242e53028614b4f00c
lothar1998 <kuglinpiotr@gmail.com>
2020-04-26 15:36:24 +0200

    [BD-22] sql script to initialize postgres database

---
+++ postgres_init.sql
@@ -0,0 +1,33 @@
+create database data_collection;
+
+\connect data_collection;
+
+create schema dc;
+
+begin;
+
+create table dc.events(
+    id serial primary key,
+    name varchar(255) not null
+);
+
+create table dc.users(
+    id serial primary key,
+    login varchar(255) not null ,
+    password varchar(255) not null
+);
+
+create table dc.sessions
+(
+    id serial primary key,
+    user_id integer not null,
+    x_coordinate integer check ( x_coordinate >= 0 ),
+    y_coordinate integer check ( y_coordinate >= 0 ),
+    event_id integer not null,
+    time timestamp not null,
+
+    foreign key (user_id) references dc.users(id),
+    foreign key (event_id) references dc.events(id)
+);
+
+commit;
