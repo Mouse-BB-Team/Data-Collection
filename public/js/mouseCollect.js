3150bf1139cded5933c64c2f38135ed797676464
kamkali <kamkalis@gmail.com>
2020-05-02 20:48:31 +0200

    Creating post endpoint for receiving single elem

--- public/js/mouseCollect.js
+++ public/js/mouseCollect.js
@@ -1,3 +1,19 @@
-document.addEventListener('click', (e) => {
-    alert("Click!!!")
-});
+const dataSendTimeout = 5000;   //send data every dataSendTimeout[ms]
+let mouseEvents = []
+
+
+sendData = function () {
+    const dataToSend = mouseEvents;
+    mouseEvents = [];
+
+    jQuery.ajax({
+        url: '/api/store-data',
+        type: 'post',
+        data: {
+            mouseEvents: JSON.stringify(dataToSend)
+        },
+        success: () => {
+            setTimeout(sendData, dataSendTimeout);
+        }
+    })
+}
--- server.js
+++ server.js
@@ -3,7 +3,24 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
+
+const collectedEventData = [];
+
+app.post('/api/store-data', (req, res) => {
+    const userEvent = {
+        x_cor: req.body.x_cor,
+        y_cor: req.body.y_cor,
+        event: req.body.event,
+        time: req.body.time
+    }
+
+    collectedEventData.push(userEvent);
+    console.log(userEvent);
+    res.status(201).end();
+});
 
 const PORT = process.env.PORT || 8080;
 
