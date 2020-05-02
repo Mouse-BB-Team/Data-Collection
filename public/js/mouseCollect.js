52618db68303bd42662cd5b2fbf982b82c709122
kamkali <kamkalis@gmail.com>
2020-05-02 22:51:35 +0200

    Getting proper JSON, for now data is sent periodically with hardcoded event type

--- public/js/mouseCollect.js
+++ public/js/mouseCollect.js
@@ -1,19 +1,43 @@
-const dataSendTimeout = 5000;   //send data every dataSendTimeout[ms]
-let mouseEvents = []
+const dataSendTimeout = 10000;   //send data every dataSendTimeout*1[ms]
+let mouseEvents = [];
 
+document.addEventListener('click', (e) => {
+    let t = new Date();
+    const clickEvent = {
+        x_cor: e.clientX,
+        y_cor: e.clientY,
+        event: 'click',//e.button,
+        time: `${t.getUTCFullYear()}-${zero_str_padding(t.getUTCMonth())}-${zero_str_padding(t.getUTCDay())} ${zero_str_padding(t.getUTCHours())}:${zero_str_padding(t.getUTCMinutes())}:${zero_str_padding(t.getUTCSeconds())}.${zero_str_padding_mls(t.getUTCMilliseconds())}`
+    }
+    mouseEvents.push(clickEvent);
+    alert(e.clientX);
+});
 
 sendData = function () {
-    const dataToSend = mouseEvents;
-    mouseEvents = [];
+    // if (mouseEvents.length !== 0){
+        const dataToSend = mouseEvents;
+        mouseEvents = [];
 
-    jQuery.ajax({
-        url: '/api/store-data',
-        type: 'post',
-        data: {
-            mouseEvents: JSON.stringify(dataToSend)
-        },
-        success: () => {
-            setTimeout(sendData, dataSendTimeout);
-        }
-    })
+        jQuery.ajax({
+            url: '/api/store-data',
+            type: 'post',
+            data: {
+                mouseEvents: JSON.stringify(dataToSend)
+            },
+            success: () => {
+                setTimeout(sendData, dataSendTimeout);
+            }
+
+        });
+    // }
+}
+
+sendData();
+
+function zero_str_padding(t) {
+    return String("0" + t).slice(-2);
+}
+
+function zero_str_padding_mls(t) {
+    return String("00" + t).slice(-3);
 }
--- server.js
+++ server.js
@@ -10,18 +10,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 const collectedEventData = [];
 
 app.post('/api/store-data', (req, res) => {
-    for (let singleEvent of req.body) {
+    const eventsList = JSON.parse(req.body.mouseEvents);
+    for (let singleEvent of eventsList) {
         const userEvent = {
             x_cor: singleEvent.x_cor,
             y_cor: singleEvent.y_cor,
             event: singleEvent.event,
             time: singleEvent.time
         }
-
         collectedEventData.push(userEvent);
     }
-
+    console.log(`Got Post no ${collectedEventData.length}`)
     res.status(201).end();
+});
+
+app.get('/api/get-data', (req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSON.stringify(collectedEventData));
 });
 
 const PORT = process.env.PORT || 8080;
