61c251c65772d676b5dbb754c95da1352ab706b0
kamkali <kamkalis@gmail.com>
2020-05-02 20:52:05 +0200

    Creating post endpoint for receiving list of mouse events from client-side

--- server.js
+++ server.js
@@ -4,21 +4,23 @@ const path = require('path');
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const collectedEventData = [];
 
 app.post('/api/store-data', (req, res) => {
-    const userEvent = {
-        x_cor: req.body.x_cor,
-        y_cor: req.body.y_cor,
-        event: req.body.event,
-        time: req.body.time
+    for (let singleEvent of req.body) {
+        const userEvent = {
+            x_cor: singleEvent.x_cor,
+            y_cor: singleEvent.y_cor,
+            event: singleEvent.event,
+            time: singleEvent.time
+        }
+
+        collectedEventData.push(userEvent);
     }
 
-    collectedEventData.push(userEvent);
-    console.log(userEvent);
     res.status(201).end();
 });
 
